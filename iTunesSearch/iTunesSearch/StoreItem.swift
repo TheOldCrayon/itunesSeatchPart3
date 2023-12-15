bec0eddddf6624b8e2ed1bff346bf8cf8d104586
TheOldCrayon <m.neumarker101@gmail.com>
2023-12-15 08:00:04 -0700

    upload
    Itunes part 3 project

---
+++ iTunesSearch/iTunesSearch/StoreItem.swift
@@ -0,0 +1,44 @@
+//
+//  StoreItem.swift
+//  iTunesSearch
+//
+//  Created by Marek Neumarker on 12/13/23.
+//
+
+import Foundation
+
+struct StoreItem: Codable {
+    let name: String
+    let artist: String
+    var description: String
+    var artworkURL: URL
+    
+    enum CodingKeys: String, CodingKey {
+        case name = "trackName"
+        case artist = "artistName"
+        case description = "longDescription"
+        case artworkURL = "artworkUrl100"
+    }
+    
+    enum AdditionalKeys: String, CodingKey {
+        case longDescription
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        name = try values.decode(String.self, forKey: CodingKeys.name)
+        artist = try values.decode(String.self, forKey: CodingKeys.artist)
+        artworkURL = try values.decode(URL.self, forKey: CodingKeys.artworkURL)
+        
+        if let description = try? values.decode(String.self, forKey: CodingKeys.description) {
+            self.description = description
+        } else {
+            let additionalValues = try decoder.container(keyedBy: AdditionalKeys.self)
+            description = (try? additionalValues.decode(String.self, forKey: AdditionalKeys.longDescription)) ?? ""
+        }
+    }
+}
+
+struct SearchResponse: Codable {
+    let results: [StoreItem]
+}
---
+++ iTunesSearch/iTunesSearch/StoreItemController.swift
@@ -0,0 +1,56 @@
+//
+//  StoreItemController.swift
+//  iTunesSearch
+//
+//  Created by Marek Neumarker on 12/13/23.
+//
+
+import Foundation
+
+import UIKit
+
+class StoreItemController {
+    func fetchItems(matching query: [String: String], completion: @escaping (Result<[StoreItem], Error>) -> Void) {
+        var urlComponents = URLComponents(string: "https://itunes.apple.com/search")!
+        urlComponents.queryItems = query.map { URLQueryItem(name: $0.key, value: $0.value) }
+        let task = URLSession.shared.dataTask(with: urlComponents.url!) { (data, response, error) in
+            if let error = error {
+                completion(.failure(error))
+            } else if let data = data {
+                do {
+                    let decoder = JSONDecoder()
+                    let searchResponse = try decoder.decode(SearchResponse.self, from: data)
+                    completion(.success(searchResponse.results))
+                } catch {
+                    completion(.failure(error))
+                }
+            }
+        }
+        
+        task.resume()
+    }
+    
+    enum PhotoInfoError: Error, LocalizedError {
+        case imageDataMissing
+    }
+    
+    func fetchImage(from url: URL, completion: @escaping
+       (Result<UIImage, Error>) -> Void) {
+        var urlComponents = URLComponents(url: url,
+               resolvingAgainstBaseURL: true)
+            urlComponents?.scheme = "https"
+        
+        let task = URLSession.shared.dataTask(with:
+               urlComponents!.url!) { (data, response, error) in
+            if let data = data, let image = UIImage(data: data) {
+                completion(.success(image))
+            } else if let error = error {
+                completion(.failure(error))
+            } else {
+                completion(.failure(PhotoInfoError.imageDataMissing))
+            }
+        }
+        task.resume()
+    }
+}
+
